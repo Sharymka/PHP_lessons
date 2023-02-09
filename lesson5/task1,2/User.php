f7b2ec1fe8b43272285d476d85beaecf37f669c2
Svetlana <sveta.sharymova@gmail.com>
2023-02-09 20:16:33 +0400

    5 | fix:  task1,2

--- lesson5/task1,2/User.php
+++ lesson5/task1,2/User.php
@@ -4,16 +4,16 @@ class User
 {
     private string $username;
     private string $email;
-
     private string $sex;
     private int $age;
     public bool $isActive = true;
     public DateTime $dateCreated;
 
-    function __construct(string $username, string $email, ?int $age = null)
+    function __construct(string $username, string $email, string $sex, ?int $age = null)
     {
         $this->username = $username;
         $this->email = $email;
+        $this->sex = $sex;
         $this->age = $age;
         $this->dateCreated = new DateTime();
     }
@@ -36,8 +36,24 @@ class User
         $this->age = $this->getValidAge($age);
     }
 
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSex(): string
+    {
+        return $this->sex;
+    }
+
 }
 
-$user = new User('Иван', 'Ivan@mail.ru', 34);
+$user = new User('Иван', 'Ivan@mail.ru', man, 34);
 echo $user->getUsername();
 //var_dump($user);
